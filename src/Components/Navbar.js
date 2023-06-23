fb8db7616e5b65e776814a6210b2085fb02717ed
Manas Patil <pmanas13092004@gmail.com>
2023-06-23 23:46:32 +0530

    Manupulate Title on Routing to different component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,20 +3,28 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function Navbar(props) {
+    const pageTitleToAbout = ()=>{
+        document.title = "TextUtile - About";
+    }
+
+    const pageTitleToHome = ()=>{
+        document.title = "TextUtils - Home";
+    }
+
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme={props.mode}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">{props.appName}</Link>
+                <Link className="navbar-brand" onClick={pageTitleToHome} to="/">{props.appName}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <Link className="nav-link active" aria-current="page" onClick={pageTitleToHome} to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">{props.aboutText}</Link>
+                            <Link className="nav-link" onClick={pageTitleToAbout} to="/about">{props.aboutText}</Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
